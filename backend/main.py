a09852fdb788a085b4f53fce06f3bb32f3eb836f
Chohyeon kim <chohyeonkim94@gmail.com>
2025-03-30 01:25:47 -0700

    feat: implement FastAPI backend for batch image upload and result polling - Added /upload endpoint to handle multiple image uploads - Generates batch_id and queues each image job for processing - Added /result/{batch_id} endpoint to return batch processing results - Starts background workers on app startup - Includes error handling during file read

---
+++ backend/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, File, UploadFile
+import uuid
+from task_runner import (
+    create_batch,
+    job_queue,
+    start_workers,
+    get_batch_result,
+)
+import time
+
+app = FastAPI()
+start_workers(workers=4)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/upload")
+async def upload_images(files:list[UploadFile] = File(...) ):
+    start_time = time.time()
+
+    batch_id = str(uuid.uuid4())
+    create_batch(batch_id, len(files))
+
+    for file in files:
+        job_id = str(uuid.uuid4())
+        try:
+            content = await file.read()
+        except Exception as e:
+            print("[upload_images] File upload Error: ", e)
+
+        job_queue.put((batch_id, job_id, content))
+        print(f" [upload_images] job_id={job_id} added. Current queue size: {job_queue.qsize()}")
+
+    # elapsed = time.time() - start_time 
+    # print(f"[upload_images] Upload and enqueue took {elapsed:.2f} seconds")
+
+    return {"batch_id": batch_id}
+
+@app.get("/result/{batch_id}")
+def batch_result(batch_id: str):
+    res = get_batch_result(batch_id)
+    return res
